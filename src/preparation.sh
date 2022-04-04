afc352e52956dae39c2902b93a41610a0f3db1e4
Christos Christidis <48795138+CChristidis@users.noreply.github.com>
2022-04-04 03:47:27 +0300

    Update preparation.sh

--- src/preparation.sh
+++ src/preparation.sh
@@ -16,6 +16,13 @@ WORKLOAD_FILES=(/root/filebench-1.5-alpha3/workloads/singlestreamread.f
 		/root/filebench-1.5-alpha3/workloads/videoserver.f)
 
 do_stuff(){
+	dirty_expire_csecs=$(< /proc/sys/vm/dirty_expire_centisecs)
+	
+	if [ $dirty_expire_csecs > $1 ]; then
+		echo "WARNING: some data may not have been written into secondary storage."
+	fi
+	
+
 	if [ -z "$(ls -A /root/workloads)" ]; then 
    		mv ${WORKLOAD_FILES[@]} /root/workloads
 	fi
