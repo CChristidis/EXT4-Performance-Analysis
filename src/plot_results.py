2172aa158d20bf84c2b9c6b004c113864110962f
Christos Christidis <48795138+CChristidis@users.noreply.github.com>
2022-04-08 02:59:40 +0300

    Update plot_results.py

--- src/plot_results.py
+++ src/plot_results.py
@@ -37,6 +37,36 @@ def plot_everything(path: str):
 		plt.plot(stat, throughput)
 		plt.show()
 
+	mean = [line[2] for line in lines]
+	ylabel = throughput[0]
+	mean = [float(elem) for idx, elem in enumerate(mean) if idx > 0]
+
+	for i in range(len(lines[0])):
+		# mean column case
+		if i == 2:
+			continue
+		stat = [line[i] for line in lines]
+		xlabel = stat[0]
+
+		if i == 4:
+			xlabel = xlabel + " (in MB)"
+		elif i == 5:
+			xlabel = xlabel + " (in KB)"
+		elif i == 6:
+			xlabel = xlabel + " (in KB)"
+
+		stat = [float(elem) for idx, elem in enumerate(stat) if idx > 0]
+
+		if sum(stat) == 0:
+			continue
+
+		plt.title("Metric: " + sys.argv[2] + ". Personality: " + sys.argv[3])
+		plt.xlabel(xlabel)
+		plt.ylabel(ylabel)
+		plt.plot(stat, throughput)
+		plt.show()
+
+
 if __name__ == "__main__":
 	path = sys.argv[1]
 	plot_everything(path)
