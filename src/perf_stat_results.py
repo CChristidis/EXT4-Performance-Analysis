63c33ec6459f4b2409f6d6b416d1f671304089c5
Christos Christidis <48795138+CChristidis@users.noreply.github.com>
2022-04-15 21:29:13 +0300

    Create perf_stat_results.py

---
+++ src/perf_stat_results.py
@@ -0,0 +1,42 @@
+import sys
+import os
+import re
+
+def get_lines(path: str):
+	file = [line for line in open(path, 'r')]
+	lines = [line.split() for line in file]
+	return lines
+
+
+def clear_unwanted_lines(lines_list):
+	lines = [elem[1:] for idx, elem in enumerate(lines_list) if len(elem) > 0 and elem[1] != "seconds"
+			 and elem[0] not in ('mke2fs', 'Performance')]
+	return lines
+
+def get_perfstat_numbers(lines):
+	results = []
+	for line in lines:
+		num = line[line.index('#') + 1]
+		num = re.sub("[^0-9^.]", "", num)
+		results.append(num)
+	return results
+	#
+
+def str(results):
+	results_str =  ", ".join(results)
+	return results_str
+
+
+
+def main(*args):
+	os.system("clear")
+	lines = get_lines("perf_stat_results.txt")
+	lines = clear_unwanted_lines(lines)
+	results = get_perfstat_numbers(lines)
+	results_str = str(results)
+	os.system("sed '1 s/.*/&, " + results_str + "/' experiment_stats.txt")
+
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
