5fddade0ee54585952d8a5840d89756df0745585
Christos Christidis <48795138+CChristidis@users.noreply.github.com>
2022-04-17 20:52:22 +0300

    Update experiment.py

--- src/experiment.py
+++ src/experiment.py
@@ -221,8 +221,11 @@ def runExperiment():
     os.system("/bin/bash /root/Desktop/stats.sh " + chosen_personality + ".f " + str(round(conf_interval, 5)) + " " +
     str(round(standard_dev, 5)) + " " + str(round(mean, 5)) + " " + str(round(float(tput[:len(tput)-4]), 5)))
 
-
-if __name__ == "__main__":
+def main():
     os.system("/bin/bash /root/scripts/stop-disk.sh")
     os.system("clear")
     runExperiment()
+    
+if __name__ == "__main__":
+  main()
+  
